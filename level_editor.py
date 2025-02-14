85699ca4278d4ac94cfa09f5b3c6932bb0770e9e
mory4ok777 <maksimsimenuk89@gmail.com>
2025-02-14 16:56:06 +0100

    Co-authored-by: ArctcShadow <ArctcShadow@users.noreply.github.com>

--- level_editor.py
+++ level_editor.py
@@ -18,20 +18,20 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Level Editor')
 
 # Завантаження зображень
-sun_img = pygame.image.load('img/sun.png')
-sun_img = pygame.transform.scale(sun_img, (tile_size, tile_size))
-bg_img = pygame.image.load('img/sky.png')
+oblothko_img = pygame.image.load("oblothko.png")
+bg_img = pygame.image.load('OIP (4).png')
 bg_img = pygame.transform.scale(bg_img, (screen_width, screen_height - margin))
-dirt_img = pygame.image.load('img/dirt.png')
-grass_img = pygame.image.load('img/grass.png')
-blob_img = pygame.image.load('img/blob.png')
-platform_x_img = pygame.image.load('img/platform_x.png')
-platform_y_img = pygame.image.load('img/platform_y.png')
-lava_img = pygame.image.load('img/lava.png')
-coin_img = pygame.image.load('img/coin.png')
-exit_img = pygame.image.load('img/exit.png')
-save_img = pygame.image.load('img/save_btn.png')
-load_img = pygame.image.load('img/load_btn.png')
+#dirt_img = pygame.image.load('dirt.png')
+grass_img = pygame.image.load('dirt.png')
+blob_img = pygame.image.load('normalslitzen.png')
+# platform_x_img = pygame.image.load('img/platform_x.png')
+# platform_y_img = pygame.image.load('img/platform_y.png')
+lava_img = pygame.image.load('LAVAnda.png')
+# coin_img = pygame.image.load('img/coin.png')
+exit_img =pygame.Surface((tile_size,int(tile_size*1.5)))#
+exit_img.fill((255,0,0))
+save_img = pygame.image.load('save_btn.png')
+load_img = pygame.image.load('load_btn.png')
 
 # Глобальні змінні
 clicked = False
@@ -45,11 +45,11 @@ font = pygame.font.SysFont('Futura', 24)
 world_data = [[0] * cols for _ in range(cols)]
 
 # Створення меж карти
-for tile in range(cols):
-    world_data[cols - 1][tile] = 2
-    world_data[0][tile] = 1
-    world_data[tile][0] = 1
-    world_data[tile][cols - 1] = 1
+# for tile in range(cols):
+#     world_data[cols - 1][tile] = 2
+#     world_data[0][tile] = 1
+#     world_data[tile][0] = 1
+#     world_data[tile][cols - 1] = 1
 
 # Функція малювання тексту
 def draw_text(text, font, text_col, x, y):
@@ -69,26 +69,26 @@ def draw_world():
             if world_data[row][col] > 0:
                 img = None
                 if world_data[row][col] == 1:
-                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
-                elif world_data[row][col] == 2:
                     img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                if world_data[row][col] == 2:
+                    img = pygame.transform.scale(oblothko_img, (tile_size, tile_size))
                 elif world_data[row][col] == 3:
                     img = pygame.transform.scale(blob_img, (tile_size, int(tile_size * 0.75)))
                     screen.blit(img, (col * tile_size, row * tile_size + (tile_size * 0.25)))
                     continue
+                # elif world_data[row][col] == 4:
+                #     img = pygame.transform.scale(platform_x_img, (tile_size, tile_size // 2))
+                # elif world_data[row][col] == 5:
+                #     img = pygame.transform.scale(platform_y_img, (tile_size, tile_size // 2))
                 elif world_data[row][col] == 4:
-                    img = pygame.transform.scale(platform_x_img, (tile_size, tile_size // 2))
-                elif world_data[row][col] == 5:
-                    img = pygame.transform.scale(platform_y_img, (tile_size, tile_size // 2))
-                elif world_data[row][col] == 6:
                     img = pygame.transform.scale(lava_img, (tile_size, tile_size // 2))
                     screen.blit(img, (col * tile_size, row * tile_size + (tile_size // 2)))
                     continue
-                elif world_data[row][col] == 7:
-                    img = pygame.transform.scale(coin_img, (tile_size // 2, tile_size // 2))
-                    screen.blit(img, (col * tile_size + (tile_size // 4), row * tile_size + (tile_size // 4)))
-                    continue
-                elif world_data[row][col] == 8:
+                # elif world_data[row][col] == 7:
+                #     img = pygame.transform.scale(coin_img, (tile_size // 2, tile_size // 2))
+                #     screen.blit(img, (col * tile_size + (tile_size // 4), row * tile_size + (tile_size // 4)))
+                #     continue
+                elif world_data[row][col] == 5:
                     img = pygame.transform.scale(exit_img, (tile_size, int(tile_size * 1.5)))
                     screen.blit(img, (col * tile_size, row * tile_size - (tile_size // 2)))
                     continue
@@ -125,7 +125,7 @@ while run:
     clock.tick(fps)
     screen.fill(green)
     screen.blit(bg_img, (0, 0))
-    screen.blit(sun_img, (tile_size * 2, tile_size * 2))
+    #screen.blit(sun_img, (tile_size * 2, tile_size * 2))
 
     if save_button.draw():
         with open(f'level{level}_data', 'wb') as pickle_out:
